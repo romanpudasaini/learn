98ec920314349bab40480d9ed1532dc61be49f02
romanpudasaini <romanpudasaini78@gmail.com>
2021-09-28 07:39:40 +0545

    i have committed

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/create', function () {
     return view('welcome');
 });
-
-Route::get('/register', function () {
-    return view('login');
-});
